6635c1fad02eb048858c24d0198af62e171a12fb
Caio H. Reblin <caioreblin07@gmail.com>
2024-08-12 19:58:07 -0300

    Base frontend

---
+++ frontend/src/components/Calendar.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Calendar from 'react-calendar';
+
+const SchedulingCalendar: React.FC = () => {
+    return (
+        <div>
+            <Calendar />
+        </div>
+    );
+};
+
+export default SchedulingCalendar;
---
+++ frontend/src/pages/AgendamentosPage.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const AgendamentosPage: React.FC = () => {
+    return (
+        <div>
+            <h1>Agendamentos</h1>
+        </div>
+    );
+};
+
+export default AgendamentosPage;
